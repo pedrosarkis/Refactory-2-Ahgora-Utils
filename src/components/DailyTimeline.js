21c3c83895b4fee32075a7ec1ded9d67d09e0711
sarkisGod <pedropverani@hotmail.com>
2021-05-16 02:06:01 -0300

    adding modal to see leave details

--- src/components/DailyTimeline.js
+++ src/components/DailyTimeline.js
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { convertDate } from '../helper/utils';
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import { flexbox } from '@material-ui/system';
+import { blockStatement } from '@babel/types';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+
+  function rand() {
+    return Math.round(Math.random() * 20) - 10;
+  }
+
+  function getModalStyle() {
+    const top = 50 + rand();
+    const left = 50 + rand();
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+
 const periodTypesTranslator = {
     shift: 'Jornada',
     nightly: 'Noturno',
@@ -20,11 +52,27 @@ const ResponseLabel = styled.label`
     color: blue;
 `
 
+const BlockLabel = styled.label`
+    display: block;
+`
+
 const PeriodWrapper = styled.div`
     border: 0px solid black;
 `
 
 const DailyTimeline = ({ timeline }) => {
+    const classes = useStyles();
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+      }
+
+      const handleOpen = () => {
+        setOpen(true);
+      };
+    
     return (
         <PeriodWrapper>
             <QuestionLabel> Inicio periodo: </QuestionLabel>
@@ -38,14 +86,36 @@ const DailyTimeline = ({ timeline }) => {
             <br></br>
             <QuestionLabel> Tipo do período: </QuestionLabel>
             <label> {timeline.type.map(type => periodTypesTranslator[type]).join(', ')} </label>
-            {/* <br/> <br/> */}
-            {/* {timeline.type.includes('leave') && (
+            {timeline.type.includes('leave') && (
                 <>
-                <QuestionLabel> Afastamento: {timeline.options.leave?.motivo} </QuestionLabel>
-                <QuestionLabel style={{display: 'block'}}> Tratamento: {timeline.options.leave.tratamento} </QuestionLabel>
+                    <Modal
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="simple-modal-title"
+                        aria-describedby="simple-modal-description"
+                    > 
+                        <div style={modalStyle} className={classes.paper}> 
+                            <BlockLabel>Motivo Afastamento: {`${timeline.options.leave.motivo}`}</BlockLabel>
+                            <BlockLabel>Tratamento: {`${timeline.options.leave.tratamento}`}</BlockLabel>
+                            <BlockLabel>Tratamento Noturno: {`${timeline.options.leave.tratamento_noturno}`}</BlockLabel>
+                            <BlockLabel>Não abona: {`${timeline.options.leave.nao_abonar_falta}`}</BlockLabel>
+                            <BlockLabel>Abona apenas ausências: {`${timeline.options.leave.apenas_abonar_ausencia}`}</BlockLabel>
+                            <BlockLabel>Força o tratamento da jornada/escala: {`${timeline.options.leave.force_shift_limits}`}</BlockLabel>
+                            <BlockLabel>Possui limites: {`${timeline.options.leave.LIMITES_HORAS_ADICIONAIS.length > 0}`}</BlockLabel>
+                            {timeline.options.leave.LIMITES_HORAS_ADICIONAIS.length > 0 && (
+                                <>
+                                     <BlockLabel>Origem: {`${timeline.options.leave.LIMITES_HORAS_ADICIONAIS[0].HORA_ADICIONAL_ORIGEM}`}</BlockLabel>
+                                     <BlockLabel>Quantidade: {`${timeline.options.leave.LIMITES_HORAS_ADICIONAIS[0].TM_QUANTIDADE}`}</BlockLabel>
+                                     <BlockLabel>Destino: {`${timeline.options.leave.LIMITES_HORAS_ADICIONAIS[0].HORA_ADICIONAL_DESTINO}`}</BlockLabel>
+                                </>
+                            ) 
+                            }
+                        </div>
+                    </Modal>
+                    <Button variant="outlined" color="primary" onClick={handleOpen}>Ver dados Afastamento</Button>
                 </>
-            )
-            } */}
+            )}
+            
         </PeriodWrapper>
     )
 }
