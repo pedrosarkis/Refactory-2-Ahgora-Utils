9df0398144e3fd0126ad50c9d55330d1a2381fd2
Pablo Bion <pablo.bion@hotmail.com>
2021-05-25 11:11:02 -0300

    Add: functions converter hours

--- src/pages/hoursConverter/index.js
+++ src/pages/hoursConverter/index.js
@@ -1,18 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {Container} from './style.js'
 import Button from "@material-ui/core/Button";
 import InputMask from 'react-input-mask';
-import MaterialInput from '@material-ui/core/Input';
 
 const HoursConverter = () => {
+    const [sexagesimal, setSexagesimal] = useState()
+    const [centesimal, setCentesimal] = useState()
+
+    const  sexagesimalRef = useRef()
+    const centesimalRef = useRef()
+
+
+    const converterToCentesimal = (hours) => {
+        hours = hours.split(':')
+        hours[1] = Math.floor(hours[1] * 1.67);
+        return `${hours[0]}.${hours[1]}`
+    }
+
+    const converterToSexagesimal = (hours) => {
+        hours = hours.split('.')
+        hours[1] = Math.floor(hours[1] / 1.67);
+        if(+hours[0] < 9) hours[0] = `0${hours[0]}`
+
+        return `${hours[0]}:${hours[1]}`
+    }
+
     return (
         <Container>
             <h1>Conversor de Horas</h1>
-
             <div>
                 <div>
                     <h3>Valor em Sexagesimal </h3>
-                    <InputMask  mask="99:99" ></InputMask>
+                    <InputMask mask="99:99" ></InputMask>
                 </div>
                 <div>
                     <h3>Valor em Sexagesimal </h3>
