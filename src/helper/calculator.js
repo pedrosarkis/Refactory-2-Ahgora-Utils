68833f21e25698813d81a3b0a74c80b45dfcffed
sarkisGod <pedropverani@hotmail.com>
2021-06-06 12:06:21 -0300

    commiting forgotten lines

--- src/helper/calculator.js
+++ src/helper/calculator.js
@@ -8,21 +8,14 @@ const decreaseHoursAndMinutes = ({hours, minutes}) => mountResult(hours.map((hou
 const sum = ({ hour1 = 0, hour2 = 0, minute1 = 0, minute2 = 0 }) => {
     const sumHours = hourToSeconds(hour1) + hourToSeconds(hour2);
     const sumMinutes = minutesToSeconds(minute1) + minutesToSeconds(minute2);
-    return {
-        hours: extractHoursFromSeconds(sumHours + sumMinutes),
-        minutes: extractMinutesFromSeconds(sumHours + sumMinutes),
-    };
+    return mountResult(sumHours + sumMinutes);
 };
 
 const decrease = ({ hour1 = 0, hour2 = 0, minute1 = 0, minute2 = 0 }) => {
     const sumFirstLine = hourToSeconds(hour1) + minutesToSeconds(minute1);
     const sumSecondLine = hourToSeconds(hour2) + minutesToSeconds(minute2);
 
-    const result = sumFirstLine - sumSecondLine;
-    return {
-        hours: extractHoursFromSeconds(result),
-        minutes: extractMinutesFromSeconds(result),
-    };
+    return mountResult(sumFirstLine - sumSecondLine);
 };
 
 const reset = (setTime) => {
